f83d664fcd477272c5a7a7d9ac9f8b58fc9dfc3c
Darsh7023 <128978068+Darsh7023@users.noreply.github.com>
2024-02-05 23:03:30 +0530

    Update Day1.js

--- Days1.js
+++ Days1.js
@@ -1,4 +1,4 @@
- // Problem 1: File Reader
+// Problem 1: File Reader
 // Problem Statement: Create a function readFileContent(filePath) that takes the path to a file as input and reads its content asynchronously using the fs module. The function should print the content to the console.
 
 const fs = require('fs')
